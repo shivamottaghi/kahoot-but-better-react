db81affcf8a5eb23ba738a35c29a483bb9928c79
shivamtg <shivamtg1376@gmail.com>
2022-08-12 15:55:43 +0200

    fix the answers bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-
+import {useEffect, useState} from 'react';
 import {fetchFromApi} from "./helpers/fetch.js";
+import parse from 'html-react-parser';
 
 /*const DisplayFetch = () => {
     const url = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple';
@@ -11,8 +11,8 @@ import {fetchFromApi} from "./helpers/fetch.js";
 }*/
 
 
-function Question(props){
-    const shuffle = (arr) =>{
+function Question(props) {
+    const shuffle = (arr) => {
         arr.sort((a, b) => 0.5 - Math.random());
         return arr;
     }
@@ -23,78 +23,86 @@ function Question(props){
     allAnswers = shuffle(allAnswers);
 
 
-
-
-    return(
-        <div className = "QuestionBox">
-            <p>{question}</p>
-            <button className='Answers' onClick={event => handleClick(allAnswers[0] === correctAnswer)}>{allAnswers[0]}</button>
-            <button className='Answers' onClick={event => handleClick(allAnswers[1] === correctAnswer)}>{allAnswers[1]}</button>
-            <button className='Answers' onClick={event => handleClick(allAnswers[2] === correctAnswer)}>{allAnswers[2]}</button>
-            <button className='Answers' onClick={event => handleClick(allAnswers[3] === correctAnswer)}>{allAnswers[3]}</button>
+    return (
+        <div className="QuestionBox">
+            <p>{parse(question)}</p>
+            <button className='Answers'
+                    onClick={event => handleClick(allAnswers[0] === correctAnswer)}>{allAnswers[0]}</button>
+            <button className='Answers'
+                    onClick={event => handleClick(allAnswers[1] === correctAnswer)}>{allAnswers[1]}</button>
+            <button className='Answers'
+                    onClick={event => handleClick(allAnswers[2] === correctAnswer)}>{allAnswers[2]}</button>
+            <button className='Answers'
+                    onClick={event => handleClick(allAnswers[3] === correctAnswer)}>{allAnswers[3]}</button>
         </div>
     );
 }
 
 function QuestionsAndAnswers() {
-    const url = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple';
-    const [questions , setQuestions] = useState([]);
-    const [questionsIsFetched , setQuestionsIsFetched] = useState(false);
-    const [answersAreSet , setAnswersAreSet] = useState(false);
-    const [selectedQuestion , setSelectedQuestion] = useState(0);
-    const [answers , setAnswers] = useState([]);
-    const [score , setScore] = useState(0);
+    const url = 'https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple';
+    const [questions, setQuestions] = useState([]);
+    const [questionsIsFetched, setQuestionsIsFetched] = useState(false);
+    const [answersAreSet, setAnswersAreSet] = useState(false);
+    const [selectedQuestion, setSelectedQuestion] = useState(0);
+    const [answers, setAnswers] = useState([]);
+    const [score, setScore] = useState(0);
 
 
-    useEffect(()=>{
-        fetchFromApi(url).then(data=> {setQuestions(data.results);
-            console.log('data',data.results);
+    useEffect(() => {
+        fetchFromApi(url).then(data => {
+            setQuestions(data.results);
+            console.log('data', data.results);
             setQuestionsIsFetched(true);
         });
 
     }, []);
 
-    useEffect(()=>{
-        if (questionsIsFetched){
+    useEffect(() => {
+        if (questionsIsFetched) {
             addAnswers()
             setAnswersAreSet(true);
         }
-    } , [questions])
+    }, [questions])
+
+    useEffect(() => {
+        addAnswers()
+    }, [selectedQuestion])
 
     const addAnswers = () => {
+
         let tempAnswers = [];
         //setAnswers(tempAnswers);
-        tempAnswers.push(questions[selectedQuestion].correct_answer);
-        questions[selectedQuestion].incorrect_answers.forEach(el => {
-           tempAnswers.push(el);
-        });
-        setAnswers([...tempAnswers]);
-        console.log("temp:" , tempAnswers);
-        console.log("answers" , answers);
+        if (questions.length){
+            tempAnswers.push(questions[selectedQuestion ].correct_answer);
+            questions[selectedQuestion ].incorrect_answers.forEach(el => {
+                tempAnswers.push(el);
+            });
+            setAnswers([...tempAnswers]);
+        }
+
+        console.log("temp:", tempAnswers);
+        console.log("answers", answers);
 
     }
 
     const handleClick = (boolResult) => {
-        if (boolResult){
-            setScore(score+1);
+        if (boolResult) {
+            setScore(score + 1);
         }
-        setSelectedQuestion(selectedQuestion + 1);
-        addAnswers();
+        setSelectedQuestion(selectedQuestion +1);
 
     }
 
-   if (questionsIsFetched && answersAreSet){
-       return(
-           <div>
-               <h5>your score : {score}</h5>
-               <Question handleClick = {handleClick} question = {questions[selectedQuestion].question}  allAnswers = {answers} correctAnswer = {questions[selectedQuestion].correct_answer}/>
-           </div>
-       );
-   }else {
-       return (
-           <p>is loading ..</p>
-       );
-   }
+    return (
+        <div>
+            <h5>your score : {score}</h5>
+            {questionsIsFetched && answersAreSet
+                ? <Question handleClick={handleClick} question={questions[selectedQuestion].question} allAnswers={answers} correctAnswer={questions[selectedQuestion].correct_answer}/>
+                : <p>is loading ..</p>
+            }
+
+        </div>
+    );
 }
 
 function App() {
@@ -102,7 +110,7 @@ function App() {
     return (
         <div className="App">
 
-            <QuestionsAndAnswers >
+            <QuestionsAndAnswers>
             </QuestionsAndAnswers>
         </div>
     );
