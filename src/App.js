7803c9c419d3ac789bbe94baa629aaf2362c17f9
shivamtg <shivamtg1376@gmail.com>
2022-08-12 10:07:22 +0200

    display one question and one answer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import {fetchFromApi} from "./helpers/fetch.js";
+
+/*const DisplayFetch = () => {
+    const url = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple';
+    fetchFromApi(url).then(res => {
+        console.log(res);
+    });
+}*/
+function QuestionAndAnswer() {
+    const url = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple';
+    const [questions , setQuestions] = useState([]);
+    const [questionsIsFetched , setQuestionsIsFetched] = useState(false);
+    const [answersAreSet , setAnswersAreSet] = useState(false);
+    const [selectedQuestion , setSelectedQuestion] = useState(0);
+    const [answers , setAnswers] = useState([]);
+
+
+    useEffect(()=>{
+        fetchFromApi(url).then(data=> {setQuestions(data.results);
+            console.log(data.results);
+            setQuestionsIsFetched(true);
+        });
+
+    }, []);
+
+    useEffect(()=>{
+        if (questionsIsFetched){
+            addAnswers()
+            setAnswersAreSet(true);
+        }
+    } , [questions])
+
+    const addAnswers = () => {
+        let tempAnswers = [];
+        tempAnswers.push(questions[selectedQuestion].correct_answer);
+        questions[selectedQuestion].incorrect_answers.forEach(el => {
+           tempAnswers.push(el);
+        });
+        setAnswers(tempAnswers);
+        console.log(answers);
+    }
+
+   if (questionsIsFetched && answersAreSet){
+       return(
+         <div>
+            <p>{questions[selectedQuestion].question}</p>
+             <ul>
+                 <li>
+                     {answers[0]}
+                 </li>
+                 <li>
+                     {answers[1]}
+                 </li>
+                 <li>
+                     {answers[2]}
+                 </li>
+                 <li>
+                     {answers[3]}
+                 </li>
+             </ul>
+         </div>
+
+       );
+   }else {
+       return (
+           <p>is loading ..</p>
+       );
+   }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <QuestionAndAnswer>
+            </QuestionAndAnswer>
+        </div>
+    );
 }
 
 export default App;
--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,5 +1,7 @@
 export async function fetchFromApi(url){
     let response = await fetch(url);
+    if (!response.ok) throw new Error(response.status);
     let data = await response.json();
     return data;
+   // return response;
 }
